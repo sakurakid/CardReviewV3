b92096e22c297e59870c3bdf7e097944832d79a1
sakurakid <610444099@qq.com>
2018-04-15 22:56:43 +0800

    第三天  加了登陆的界面

---
+++ app/src/main/java/com/example/hasee/cardreviewv3/activity/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.example.hasee.cardreviewv3.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.hasee.cardreviewv3.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
