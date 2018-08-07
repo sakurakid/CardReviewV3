10425fc7f3d4d1dba38a5f03fbcfed50477c5f41
sakurakid <610444099@qq.com>
2018-08-07 22:06:40 +0800

    正在完成添加卡包这块UI

---
+++ app/src/main/java/com/example/hasee/cardreviewv3/BagFragmentActivitise/AddBag.java
@@ -0,0 +1,15 @@
+package com.example.hasee.cardreviewv3.BagFragmentActivitise;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.hasee.cardreviewv3.R;
+
+public class AddBag extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_bag);
+    }
+}
--- app/src/main/java/com/example/hasee/cardreviewv3/fragment/bagFragment.java
+++ app/src/main/java/com/example/hasee/cardreviewv3/fragment/bagFragment.java
@@ -1,12 +1,15 @@
 package com.example.hasee.cardreviewv3.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import com.example.hasee.cardreviewv3.BagFragmentActivitise.AddBag;
 import com.example.hasee.cardreviewv3.R;
 
 /**
@@ -15,9 +18,29 @@ import com.example.hasee.cardreviewv3.R;
  */
 
 public class bagFragment extends Fragment {
+    private View view;
+    private ImageView addbag;//添加卡包
+    private ImageView update;//更新卡包数据
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_bag,container, false);
+        view = inflater.inflate(R.layout.fragment_bag,container, false);
+        initView(container);
+        return view;
+    }
+    /**
+     * 绑定
+     */
+    private void initView(ViewGroup c){
+        addbag = (ImageView) view.findViewById(R.id.iv_add_bag);
+        update = (ImageView) view.findViewById(R.id.iv_upload_bag);
+
+        addbag.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), AddBag.class);
+                startActivity(intent);
+            }
+        });
     }
 }
